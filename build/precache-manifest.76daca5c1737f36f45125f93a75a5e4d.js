c12aaaef71ce2b31234c2a807340e0a6ab5a8c17
MAKSYM_enko <finside41@gmail.com>
2019-03-13 00:15:33 +0200

    deploy1

---
+++ build/precache-manifest.76daca5c1737f36f45125f93a75a5e4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/data-table-react/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "20153f80517951a7fcc6",
+    "url": "/data-table-react/build/static/js/runtime~main.e16f3020.js"
+  },
+  {
+    "revision": "bae39b7c2693e9b6d984",
+    "url": "/data-table-react/build/static/js/main.57a08ddc.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/data-table-react/build/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "bae39b7c2693e9b6d984",
+    "url": "/data-table-react/build/static/css/main.483fb92f.chunk.css"
+  },
+  {
+    "revision": "42163815ec1371cc82da7fd7afa7f769",
+    "url": "/data-table-react/build/index.html"
+  }
+];
